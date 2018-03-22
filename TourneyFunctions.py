69f6a9d8d59d08c47e796e76a96960c25c7bf167
Francisco Rey Zaluar <franciscoreyzaluar@gmail.com>
2018-03-22 16:43:02 +0100

    Update TourneyFunctions.py

--- TourneyFunctions.py
+++ TourneyFunctions.py
@@ -11,6 +11,18 @@ import random as rd
 
 
 # fonction définissant l'arbre du tournoi
+    # l'arbre sera en fait une liste, qui commencera avec tous les participants et
+    # de laquelle on enlèver les perdants à chaque round. A la fin ce sera un singleton.
+def InitializeTree(participants):
+    Tourney = participants
+    rd.shuffle(Tourney)
+    return Tourney
+            
+def ActualizeTree(Tourney, perdants):
+    for chunin in perdants:
+        Tourney.remove(perdants)
+    return Tourney
 
 
-# fonction interface de combat 
+# fonction interface (afficher)
+    # gameboard, mouvements, actions...
