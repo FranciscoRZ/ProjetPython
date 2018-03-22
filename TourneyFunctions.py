79cc645eacbf359fda5b292bd6c64e6a340d6f34
Francisco Rey Zaluar <franciscoreyzaluar@gmail.com>
2018-03-22 16:01:02 +0100

    Fonctions du tournoi (combat, afficher, etc.)

---
+++ TourneyFunctions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  8 18:28:14 2018
+
+@author: Cisco
+"""
+import random as rd
+
+# fonction combat
+
+
+# fonction définissant l'arbre du tournoi
+
+
+# fonction interface de combat 
